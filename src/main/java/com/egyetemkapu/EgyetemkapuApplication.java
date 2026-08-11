dba4436899a7125f20217194946933848d8dbc38
Czeitner András <czeitnerandras@mailbox.unideb.hu>
2026-08-11 11:37:14 +0200

    Initial commit

---
+++ src/main/java/com/egyetemkapu/EgyetemkapuApplication.java
@@ -0,0 +1,13 @@
+package com.egyetemkapu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgyetemkapuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgyetemkapuApplication.class, args);
+	}
+
+}
