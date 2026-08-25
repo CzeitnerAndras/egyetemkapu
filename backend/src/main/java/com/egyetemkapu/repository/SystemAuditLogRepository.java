8e0703e3b3f713dae2ed8d7f52627dcde3601251
Czeitner András <czeitnerandras82@gmail.com>
2026-08-25 20:20:10 +0200

    feat: create SystemAuditLogRepository for database interactions

---
+++ backend/src/main/java/com/egyetemkapu/repository/SystemAuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.egyetemkapu.repository;
+
+import com.egyetemkapu.model.SystemAuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SystemAuditLogRepository extends JpaRepository<SystemAuditLog, Long> {
+}
