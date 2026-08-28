78f4c8962669d72a8f7f3f507f23431018ee3ee5
Czeitner András <czeitnerandras82@gmail.com>
2026-08-28 12:18:01 +0200

    feat: add Jest configuration for TypeScript testing

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }],
+  },
+};
